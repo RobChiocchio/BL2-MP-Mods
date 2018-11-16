80d4e503a5d9ace2802d242c5ca9b58babc4a435
Rob Chiocchio <r.mchiocchio@gmail.com>
2018-11-16 15:31:58 -0500

    Electron soon?

---
+++ Installer/app/scripts/titlebar.js
@@ -0,0 +1,31 @@
+(function(){
+    const electron = require('electron');
+    const remote = electron.remote;
+
+    function init() {
+        document.getElementById("buttonMinimize").addEventListener("click", function(e) {
+            const window = remote.getCurrentWindow();
+            window.minimize();
+        });
+
+        document.getElementById("buttonMaximize").addEventListener("click", function(e) {
+            const window = remote.getCurrentWindow();
+            if (!window.isMaximized()) {
+                window.maximize();
+            } else {
+                window.unmaximize();
+            }
+        });
+
+        document.getElementById("buttonClose").addEventListener("click", function(e) {
+            const window = remote.getCurrentWindow();
+            window.close();
+        });
+    };
+
+    document.onreadystatechange = function () {
+        if (document.readyState == "complete") {
+            init();
+        }
+    };
+})();
---
+++ Installer/main.js
@@ -0,0 +1,58 @@
+'use strict'; // don't ignore bad code
+
+const electron = require('electron');
+const app = electron.app; // control app
+const BrowserWindow = electron.BrowserWindow; // create native browser window
+const ipcMain = electron.ipcMain;
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 430,
+    height: 360,
+    minWidth: 430,
+    minHeight: 230,
+    frame: false, // remove frame from windows apps
+    titleBarStyle: 'hidden' // hide mac titlebar
+  });
+
+  mainWindow.setIcon
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('app/index.html');
+
+  // Open the DevTools.
+  //mainWindow.webContents.openDevTools();
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') { // OSX quit fix
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
