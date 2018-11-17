3de3d12206d30c54c8210f026b3a2fb8c31535fe
Rob Chiocchio <RobethX@users.noreply.github.com>
2018-11-17 17:27:41 -0500

    fixed tabs

--- Installer/app/scripts/patch.js
+++ Installer/app/scripts/patch.js
@@ -1,14 +1,7 @@
-
-
-function updateLoadingBar(percent){
-    var loadingBarProgress = document.getElementById("loadingBarProgress");
-    loadingBarProgress.style.width = percent + '%';
-};
-
 function extractUPK(filename){
 
-};
+}
 
 function patch(){ //main patch function? or call DLL
 
-};
+}
