6de5095754a4a83c33b29f8b2bd2bb8e3c711fca
Rob Chiocchio <r.mchiocchio@gmail.com>
2018-12-12 15:31:38 -0500

    Make seperate script for hexediting

---
+++ scripts/hexedit.js
@@ -0,0 +1,25 @@
+const _ = require('lodash');
+const Promise = require("bluebird");
+
+if (require.main === module) { // If not just an import
+    standalone();
+}
+
+function edit(filePath, modName) { // Check list of mods and apply modName to filePath // (or something like that) TODO
+
+}
+
+function standalone() { // TODO: do standalone script
+    var meow = require("meow");
+
+    // Process arguments
+    let args = meow("\
+        Usage\
+            $ hexedit <file> <mod>\
+\
+        Examples\
+            $ hexedit \"WillowGame\\CookedPCConsole\\WillowGame.upk\" debugMenu\
+    ");
+
+    edit
+}
